9d5b398bed08a58a3cc03c7755cc69f0a34f9de2
Ashley Campbell <agdc101@gmail.com>
2020-05-01 12:30:17 +0100

    Day 6 Search bar functioning!
    Ready for review

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 
-
+// -------- SEARCH BAR FUNCTION -------- //
 $('input').on('keyup', () => {
-
     $('a').each( function (){
-        let search = $('input').val();
-        let caption = $(this).attr('data-title');
-        let image = $(this);
-
+        let search = $('input').val().toLowerCase();
+        let caption = $(this).attr('data-title').toLowerCase();
+        // if the caption isnt included. hide() the image             
         if ( !caption.includes(search) ) {
-            console.log('hooray');
-
-            // if the caption isnt included. hide() the image
+            $(this).fadeOut(600);           
+        } // show function added if user removes values to the input. 
+        else {
+            $(this).fadeIn(600);
         }
        
     });
